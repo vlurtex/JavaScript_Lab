6d64464064bb2a9576ac762b577b06e7916499fa
vlurtex <98977938+vlurtex@users.noreply.github.com>
2022-09-30 16:54:59 +0300

    Update lab_2.4.js

--- lab_2/lab_2.4.js
+++ lab_2/lab_2.4.js
@@ -2,8 +2,8 @@ function fib(n){
         if (n<2){
             return n;
         }else{
-            return fib(n-1)+fib(n-2);
+            return bigInt(fib(n-1)+fib(n-2));
         } 
     }
 let n = prompt("Введите n: ")
-alert(bigInt(fib(n)))
+alert(fib(n))
